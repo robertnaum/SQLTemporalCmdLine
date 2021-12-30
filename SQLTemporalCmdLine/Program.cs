ad9c70bc9ec6d60cfdf6851f2f5207a42084de75
Robert Naum <robert@kellyreg.com>
2021-12-30 01:16:17 -0500

    initial commit

--- SQLTemporalCmdLine/Program.cs
+++ SQLTemporalCmdLine/Program.cs
@@ -1,13 +1,109 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Cocona;
+using Cocona.Filters;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace SQLTemporalCmdLine
 {
     class Program
     {
+        const string DEFAULT_SETTINGS_JSON_FILENAME = "appSettings.json";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            (string settingsFile, string[] remainingArgs) = ExtractGlobalOptions(args);
+
+            if (!System.IO.File.Exists(settingsFile))
+            {
+                Console.WriteLine($"ERROR: Cannot build configuration using '{settingsFile}' settings file");
+                return;
+            }
+
+            CoconaApp.Create()
+                .ConfigureAppConfiguration(cfg => {
+                    if (settingsFile != DEFAULT_SETTINGS_JSON_FILENAME)
+                        cfg.AddJsonFile(settingsFile);
+                    cfg.AddCommandLine(remainingArgs);
+                    cfg.Build();
+                    })
+                .Run<Program>(remainingArgs, options => {
+                    options.TreatPublicMethodsAsCommands = false;
+                    options.EnableConvertCommandNameToLowerCase = false;
+                    options.EnableConvertOptionNameToLowerCase = false;
+            });
         }
+        
+        private static (string settingsFile, string[] remainingArgs) ExtractGlobalOptions(string[] args)
+        {
+            string settingsFile = DEFAULT_SETTINGS_JSON_FILENAME;
+            List<string> newArgs = new List<string>();
+            if (args != null && args.Length > 0)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {  
+                    string arg = args[i].ToLower();   //peel off verbose flag, assign to global state
+                    if (arg.Contains("--appSettings") || arg.Contains("-a"))  
+                    {
+                        var arr = arg.Split('=');  //--appSettings=somefile.json
+                        if (arr != null && arr.Length > 1 && !string.IsNullOrEmpty(arr[0]) && !string.IsNullOrEmpty(arr[1]))
+                        {
+                            settingsFile = arr[1];
+                        }
+                    }
+                    else
+                    {
+                        newArgs.Add(arg);
+                    }
+                }
+
+            }
+            return (settingsFile, newArgs.ToArray());
+        }
+
+        //[IgnoreUnknownOptions]
+        [Command(Description = "add a column to a specific table")]
+        public void add( [FromService] IConfiguration config,
+                         [Argument (Description = "connection string key in configuration")] string ConnectionStringKey,
+                         [Argument (Description = "name of table")] string TableName,
+                         [Argument (Description = "name of column to add")] string ColumnName,
+                         [Argument (Description = "type of column")] string ColumnType,
+                         [Option('n', Description = "is column nullable? (default=no)")] bool nullable = false,
+                         [Option('d', Description = "default value")] string @default = ""
+                         )
+        {
+            var connectionString = config.GetConnectionString(ConnectionStringKey);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                Console.WriteLine($"Cannot find ConnectionStringKey '{ConnectionStringKey}'");
+            }
+            else
+            {            
+                var sql = new SQLServer(config, connectionString);
+                sql.AddColumn(TableName, ColumnName, ColumnType, nullable, @default);
+            }
+        }
+
+        //[IgnoreUnknownOptions]
+        [Command(Description = "delete a column from a specific table")]
+        public void delete( [FromService] IConfiguration config,
+                            [Argument (Description = "connection string key in configuration")] string ConnectionStringKey,
+                            [Argument (Description = "name of table")] string TableName,
+                            [Argument(Description = "name of column to delete")]string ColumnName)
+        {
+            var connectionString = config.GetConnectionString(ConnectionStringKey);
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                Console.WriteLine($"Cannot find ConnectionStringKey '{ConnectionStringKey}'");
+            }
+            else
+            {            
+                var sql = new SQLServer(config, connectionString);
+                sql.DeleteColumn(TableName, ColumnName);
+            }
+            Console.WriteLine($"deleted column name [{ColumnName}] from table [{TableName}]");
+        }
+
     }
+
 }
---
+++ SQLTemporalCmdLine/SQLServer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLTemporalCmdLine
+{
+    class SQLServer
+    {
+        private string _connectionString;
+        private IConfiguration _config;
+        private bool verbose;
+        public SQLServer(IConfiguration config, string connectionString)
+        {
+            _connectionString = connectionString;
+            _config = config;
+            verbose = Convert.ToBoolean(config.GetSection("verbose").Value);
+        }
+        public void AddColumn(string TableName, string ColumnName, string ColumnType, bool nullable, string @default)
+        {
+//            StringBuilder sb = new StringBuilder();
+            using (var con = new SqlConnection(_connectionString))
+            {
+
+            }
+
+            if (verbose)
+            {
+                Console.Write($"added column name [{ColumnName}] to table [{TableName}]");
+                Console.Write($" of type {ColumnType} ");
+                Console.Write($"{(nullable ? "NULL" : "NOT NULL")}");
+                if (!string.IsNullOrEmpty(@default))
+                    Console.Write ($" DEFAULT \"{@default}\"");
+                Console.WriteLine();
+            }
+
+        }
+
+        public void DeleteColumn(string TableName, string ColumnName)
+        {
+            using (var con = new SqlConnection(_connectionString))
+            {
+                con.Open();
+                string sql = $"ALTER TABLE {TableName} DROP COLUMN {ColumnName}";
+                using (var cmd = new SqlCommand(sql, con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+    }
+}
